0ddacdb8c7556b8e143007e37cc1942ccb765e2d
Tatu Tarvainen <tatutarvainen@82-128-130-38.rev.dnainternet.fi>
2021-01-10 21:30:32 +0200

    Make atom source into a defrecord

--- src/ripley/live/atom.clj
+++ src/ripley/live/atom.clj
@@ -3,25 +3,33 @@
   (:require [ripley.live.protocols :as p]
             [clojure.core.async :as async :refer [go >!! >!]]))
 
-(defn atom-source [source-atom]
-  (let [key (java.util.UUID/randomUUID)
-        channels (atom #{})]
+(defrecord AtomSource [source-atom channels process-value]
+  p/Source
+  (to-channel [_]
+    (let [ch (async/chan 1
+                         (when process-value
+                           (map process-value)))]
+      (>!! ch @source-atom)
+      (swap! channels conj ch)
+      ch))
 
-    (add-watch source-atom key
-               (fn [_ _ old-value new-value]
-                 (when (not= old-value new-value)
-                   ;;(println source-atom " value: " old-value " => " new-value)
-                   (go
-                     (doseq [ch @channels]
-                       (>! ch new-value))))))
-    (reify p/Source
-      (to-channel [_]
-        (let [ch (async/chan 1)]
-          (>!! ch @source-atom)
-          (swap! channels conj ch)
-          ch))
-      (immediate? [_] true)
-      (close! [_]
-        (remove-watch source-atom key)
-        (doseq [ch @channels]
-          (async/close! ch))))))
+  (immediate? [_] true)
+
+  (close! [_]
+    (remove-watch source-atom key)
+    (doseq [ch @channels]
+      (async/close! ch))))
+
+(defn atom-source
+  ([source-atom]
+   (atom-source source-atom nil))
+  ([source-atom process-value]
+   (let [key (java.util.UUID/randomUUID)
+         channels (atom #{})]
+     (add-watch source-atom key
+                (fn [_ _ old-value new-value]
+                  (when (not= old-value new-value)
+                    (go
+                      (doseq [ch @channels]
+                        (>! ch new-value))))))
+     (->AtomSource source-atom channels process-value))))
