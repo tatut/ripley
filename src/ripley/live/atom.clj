d05b3da9365164f646e57f84569c96756cff5a79
Tatu Tarvainen <tatutarvainen@82-128-130-38.rev.dnainternet.fi>
2021-01-12 17:59:04 +0200

    atom-source back to single channel
    As to-channel is expected to be able to call multiple times, that
    caused problems elsewhere. Many sources can be created from the same atom.

--- src/ripley/live/atom.clj
+++ src/ripley/live/atom.clj
@@ -3,33 +3,29 @@
   (:require [ripley.live.protocols :as p]
             [clojure.core.async :as async :refer [go >!! >!]]))
 
-(defrecord AtomSource [source-atom channels process-value]
+(defrecord AtomSource [key source-atom channel]
   p/Source
-  (to-channel [_]
-    (let [ch (async/chan 1
-                         (when process-value
-                           (map process-value)))]
-      (>!! ch @source-atom)
-      (swap! channels conj ch)
-      ch))
-
+  (to-channel [_] channel)
   (immediate? [_] true)
-
   (close! [_]
     (remove-watch source-atom key)
-    (doseq [ch @channels]
-      (async/close! ch))))
+    (async/close! channel)))
 
 (defn atom-source
+  "Create a source that tracks changes made to the given atom.
+  The source will return the same channel on each call to to-channel
+  so the same source can't be used for multiple live components."
   ([source-atom]
    (atom-source source-atom nil))
   ([source-atom process-value]
    (let [key (java.util.UUID/randomUUID)
-         channels (atom #{})]
+         ch (async/chan 1
+                        (when process-value
+                          (map process-value)))]
      (add-watch source-atom key
                 (fn [_ _ old-value new-value]
                   (when (not= old-value new-value)
                     (go
-                      (doseq [ch @channels]
-                        (>! ch new-value))))))
-     (->AtomSource source-atom channels process-value))))
+                      (>! ch new-value)))))
+     (>!! ch @source-atom)
+     (->AtomSource key source-atom ch))))
