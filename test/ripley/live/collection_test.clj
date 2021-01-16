9ac3d785bab164aec26a06eaaeee47b7df2e4a10
Tatu Tarvainen <tatutarvainen@82-128-130-38.rev.dnainternet.fi>
2021-01-16 21:26:15 +0200

    Refactor live collection setup for reuse

--- test/ripley/live/collection_test.clj
+++ test/ripley/live/collection_test.clj
@@ -20,7 +20,10 @@
        ~@body
        (.toString out#))))
 
-(deftest collection-update-process
+(defn- take! [sent-ch]
+  (first (alts!! [sent-ch (async/timeout 500)])))
+
+(defn setup-live-collection []
   (let [sent-ch (async/chan 10)
         ctx (test-context sent-ch)
         items (atom [])]
@@ -40,50 +43,58 @@
 
       ;; Start the live context
       (async/close! (:wait-ch @(:state ctx)))
-      
-      (doseq [[value expected-patch]
 
-              [;; Add item, sends patch to prepend item
-               [[{:k 1 :v 1}]
-                "0:P:1=1/1"]
+      ;; Return handles needed to test the live collection handling
+      {:sent-ch sent-ch
+       :items items
+       :set-items!
+       (fn [[new-items expected-patches]]
+         (reset! items new-items)
+         (doseq [patch (if (string? expected-patches)
+                         [expected-patches]
+                         expected-patches)]
+           (is (= (take! sent-ch) patch))))})))
 
-               ;; Adding second item send patch to add if after first
-               [[{:k 1 :v 1} {:k 2 :v 2}]
-                "1:F:2=2/2"]
+(deftest collection-update-process
+  (let [{:keys [set-items!]} (setup-live-collection)]
+    (doseq [items-patches
+            
+            [;; Add item, sends patch to prepend item
+             [[{:k 1 :v 1}]
+              "0:P:1=1/1"]
 
-               ;; Add third, append after second
-               [[{:k 1 :v 1} {:k 2 :v 2} {:k 3 :v 3}]
-                "2:F:3=3/3"]
-               
-               ;; Remove second item, sends deletion patch
-               [[{:k 1 :v 1} {:k 3 :v 3}]
-                "2:D"]
+             ;; Adding second item send patch to add if after first
+             [[{:k 1 :v 1} {:k 2 :v 2}]
+              "1:F:2=2/2"]
 
-               ;; Change value of third item
-               [[{:k 1 :v 1} {:k 3 :v "FIXED"}]
-                "3:R:3=3/FIXED"]
-               
-               ;; Add fourth item add end, append after third
-               [[{:k 1 :v 1} {:k 3 :v "FIXED"} {:k 4 :v 4}]
-                "3:F:4=4/4"]
+             ;; Add third, append after second
+             [[{:k 1 :v 1} {:k 2 :v 2} {:k 3 :v 3}]
+              "2:F:3=3/3"]
+             
+             ;; Remove second item, sends deletion patch
+             [[{:k 1 :v 1} {:k 3 :v 3}]
+              "2:D"]
 
-               ;; Prepend fifth item
-               [[{:k 5 :v 5} {:k 1 :v 1} {:k 3 :v "FIXED"} {:k 4 :v 4}]
-                "0:P:5=5/5"]
-               
-               ;; Add sixth between 3 and 4
-               [[{:k 5 :v 5} {:k 1 :v 1} {:k 3 :v "FIXED"} {:k 6 :v 6} {:k 4 :v 4}]
-                "3:F:6=6/6"]
+             ;; Change value of third item
+             [[{:k 1 :v 1} {:k 3 :v "FIXED"}]
+              "3:R:3=3/FIXED"]
+             
+             ;; Add fourth item add end, append after third
+             [[{:k 1 :v 1} {:k 3 :v "FIXED"} {:k 4 :v 4}]
+              "3:F:4=4/4"]
 
-               ;; prepend and append on same change
-               [[{:k "b+" :v 666}
-                 {:k 5 :v 5} {:k 1 :v 1} {:k 3 :v "FIXED"} {:k 6 :v 6} {:k 4 :v 4} 
-                 {:k "b-" :v -666}]
-                ["0:P:7=b+/666"
-                 "4:F:8=b-/-666"]]]]
-        
-        (reset! items value)
-        (doseq [patch (if (string? expected-patch)
-                        [expected-patch]
-                        expected-patch)]
-          (is (= (first (alts!! [sent-ch (async/timeout 500)])) patch)))))))
+             ;; Prepend fifth item
+             [[{:k 5 :v 5} {:k 1 :v 1} {:k 3 :v "FIXED"} {:k 4 :v 4}]
+              "0:P:5=5/5"]
+             
+             ;; Add sixth between 3 and 4
+             [[{:k 5 :v 5} {:k 1 :v 1} {:k 3 :v "FIXED"} {:k 6 :v 6} {:k 4 :v 4}]
+              "3:F:6=6/6"]
+
+             ;; prepend and append on same change
+             [[{:k "b+" :v 666}
+               {:k 5 :v 5} {:k 1 :v 1} {:k 3 :v "FIXED"} {:k 6 :v 6} {:k 4 :v 4} 
+               {:k "b-" :v -666}]
+              ["0:P:7=b+/666"
+               "4:F:8=b-/-666"]]]]
+      (set-items! items-patches))))
