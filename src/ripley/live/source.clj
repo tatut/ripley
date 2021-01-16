126260bbd038ca81adbd49d5c351fa2e346c36a3
Tatu Tarvainen <tatutarvainen@82-128-130-38.rev.dnainternet.fi>
2021-01-16 10:35:53 +0200

    Map source util

--- src/ripley/live/source.clj
+++ src/ripley/live/source.clj
@@ -2,7 +2,8 @@
   "Convert to source"
   (:require [ripley.live.protocols :as p]
             ripley.live.atom
-            ripley.live.async))
+            ripley.live.async
+            [clojure.core.async :as async :refer [go-loop <! >!]]))
 
 (defmulti to-source class)
 
@@ -18,3 +19,19 @@
   (if (satisfies? p/Source x)
     x
     (to-source x)))
+
+(defn map-source
+  "Returns new source that whose value is processed with fun."
+  [fun source]
+  (let [orig-channel (p/to-channel source)
+        ch (async/chan 1)]
+    (go-loop [v (<! orig-channel)]
+      (if (nil? v)
+        (async/close! ch)
+        (let [mapped-value (fun v)]
+          (>! ch mapped-value)
+          (recur (<! orig-channel)))))
+    (reify p/Source
+      (to-channel [_] ch)
+      (immediate? [_] (p/immediate? source))
+      (close! [_] (p/close! source)))))
