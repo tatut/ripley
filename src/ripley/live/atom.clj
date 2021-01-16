b0a15f69935430bdbb411e64e1cbb3382c9f189d
Tatu Tarvainen <tatutarvainen@82-128-130-38.rev.dnainternet.fi>
2021-01-17 00:10:43 +0200

    Dedupe values for atom source that has process-value

--- src/ripley/live/atom.clj
+++ src/ripley/live/atom.clj
@@ -1,7 +1,8 @@
 (ns ripley.live.atom
   "Live component that tracks an atom's value"
   (:require [ripley.live.protocols :as p]
-            [clojure.core.async :as async :refer [go >!! >!]]))
+            [clojure.core.async :as async :refer [go >!! >!]]
+            [taoensso.timbre :as log]))
 
 (defrecord AtomSource [key source-atom channel]
   p/Source
@@ -15,17 +16,30 @@
   "Create a source that tracks changes made to the given atom.
   The source will return the same channel on each call to to-channel
   so the same source can't be used for multiple live components."
-  ([source-atom]
-   (atom-source source-atom nil))
-  ([source-atom process-value]
-   (let [key (java.util.UUID/randomUUID)
+  ([atom-or-opts]
+   (let [{:keys [atom process-value label]}  (if (map? atom-or-opts)
+                                              atom-or-opts
+                                              {:atom atom-or-opts})
+         key (java.util.UUID/randomUUID)
          ch (async/chan 1
                         (when process-value
-                          (map process-value)))]
-     (add-watch source-atom key
+                          ;; When process value is specified we must also
+                          ;; deduplicate as the processed value may not
+                          ;; change even if atom value changes and we
+                          ;; don't want to rerender then.
+                          (comp (map process-value)
+                                (dedupe)))
+                        (fn [ex]
+                          (log/warn ex "Exception in atom source channel")))]
+     (add-watch atom key
                 (fn [_ _ old-value new-value]
                   (when (not= old-value new-value)
+                    (when label
+                      (log/debug label "atom-source changed, v:" new-value))
                     (go
                       (>! ch new-value)))))
-     (>!! ch @source-atom)
-     (->AtomSource key source-atom ch))))
+     (>!! ch @atom)
+     (->AtomSource key atom ch)))
+  ([atom process-value]
+   (atom-source {:atom atom
+                 :process-value process-value})))
