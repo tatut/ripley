80b1b999273ed82c1f7bf6ccb33ffeb8feb1fd82
Tatu Tarvainen <tatutarvainen@82-128-130-38.rev.dnainternet.fi>
2021-05-01 22:51:33 +0300

    Don't mangle SVG attribute names

--- src/ripley/html.clj
+++ src/ripley/html.clj
@@ -56,10 +56,42 @@
                            "onkeypress" "onkeyup" "onkeydown"
                            "ondblclick"})
 
+(def no-mangle-attributes
+  "Attribute names that should not be mangled (like SVG attrs having dashes)"
+  #{"accent-height" "alignment-baseline"
+    "arabic-form" "baseline-shift" "cap-height"
+    "clip-path" "clip-rule" "color-interpolation"
+    "color-interpolation-filters" "color-profile"
+    "color-rendering" "dominant-baseline"
+    "enable-background" "fill-opacity" "fill-rule"
+    "flood-color" "flood-opacity" "font-family"
+    "font-size" "font-size-adjust" "font-stretch"
+    "font-style" "font-variant" "font-weight"
+    "glyph-name" "glyph-orientation-horizontal"
+    "glyph-orientation-vertical"
+    "horiz-adv-x" "horiz-origin-x" "image-rendering"
+    "letter-spacing" "lighting-color"
+    "marker-end" "marker-mid" "marker-start"
+    "overline-position" "overline-thickness"
+    "panose-1" "paint-order" "pointer-events"
+    "rendering-intent" "shape-rendering"
+    "stop-color" "stop-opacity" "strikethrough-position"
+    "strikethrough-thickness" "stroke-dasharray"
+    "stroke-dashoffset" "stroke-linecap" "stroke-linejoin"
+    "stroke-miterlimit" "stroke-opacity" "stroke-width"
+    "text-anchor" "text-decoration" "text-rendering"
+    "transform-origin" "underline-position"
+    "underline-thickness" "unicode-bidi" "unicode-range"
+    "units-per-em" "v-alphabetic" "v-hanging"
+    "v-ideographic" "v-mathematical" "vector-effect"
+    "vert-adv-y" "vert-origin-x" "vert-origin-y"
+    "word-spacing" "writing-mode" "x-height"})
+
 (defn- html-attr-name [attr-name]
   (let [name (name attr-name)]
-    (if (str/starts-with? name "data-")
-      ;; Keep data attribute names as is
+    (if (or (str/starts-with? name "data-")
+            (no-mangle-attributes name))
+      ;; Keep data attribute names and names marked no mangle as is
       name
 
       ;; otherwise lowercase and remove dashes
