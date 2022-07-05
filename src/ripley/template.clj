9913edcd3a51f920f36d7f37baf44e815c7f60d9
Tatu Tarvainen <tatu.tarvainen@me.com>
2022-07-05 07:19:32 +0300

    Make template data proxy public.
    Template data proxy can be used in other templating.

--- src/ripley/template.clj
+++ src/ripley/template.clj
@@ -9,7 +9,7 @@
 ;; Define a proxy map that automatically creates names for
 ;; keys that are accessed from the map.
 ;; The same key will return the same name.
-(deftype TemplateDataProxy [state]
+(deftype TemplateDataProxy [format-pattern state]
   clojure.lang.ILookup
   (valAt [this key]
     (let [new-state
@@ -19,13 +19,31 @@
                      state
                      {::idx (inc idx)
                       ::fields (assoc fields key idx)})))]
-      (str "{{_rl" (-> new-state ::fields key) "}}")))
+      (format format-pattern (-> new-state ::fields key))))
   (valAt [this key not-found]
     (.valAt this key)))
 
-(defn- ->template-data-proxy []
-  (TemplateDataProxy. (atom {::idx 0 ::fields {}})))
+(defn ->template-data-proxy
+  "Create a new template data proxy that tracks what
+  fields are accessed.
 
+  When a field is accessed, it is given an index and
+  a template reference name for it is returned.
+
+  Optionally takes a format pattern on how to generate
+  the name. Defaults to \"{{_rl%d}}\" where %d is replaced
+  with the index."
+  ([] (->template-data-proxy "{{_rl%d}}"))
+  ([format-pattern]
+   (TemplateDataProxy. format-pattern
+                       (atom {::idx 0 ::fields {}}))))
+
+(defn template-data-proxy-fields
+  "Take all the accessed fields of the template data proxy
+  and return a function that returns all those fields from the
+  given map as a vector."
+  [tdp]
+  (apply juxt (map key (sort-by val (::fields @(.-state tdp))))))
 
 (defn use-template
   "Render a template based on the given component.
@@ -41,7 +59,7 @@
         dp (->template-data-proxy)
         id (when ctx (p/register! ctx nil :_ignore {}))
         body (output/render-to-string component dp)
-        fields (apply juxt (map key (sort-by val (::fields @(.-state dp)))))]
+        fields (template-data-proxy-fields dp)]
     (p/listen! data-source
                #(p/send! ctx [[id "T" (into [selector] (map fields) %)]]))
     (h/html [:template {:data-rl id}
