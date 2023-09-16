c1f9e8d076ba060eaf5e732a085c97a1c36997d9
Tatu Tarvainen <tatu.tarvainen@me.com>
2023-09-16 17:13:26 +0300

    And pedestal

--- build.clj
+++ build.clj
@@ -11,7 +11,7 @@
 (def basis
   (b/create-basis {:project "deps.edn"
                    ;; Add all optional dependencies as well, so cljdoc analysis succeeds
-                   :aliases [:manifold :redis :xtdb]}))
+                   :aliases [:manifold :redis :xtdb :pedestal]}))
 
 (def jar-file "target/ripley.jar") ;(format "target/%s-%s.jar" (name lib) version)
 
