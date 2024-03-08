c250e1f83ede80cb0da53a286d7049076600a132
Tatu Tarvainen <tatu.tarvainen@solita.fi>
2024-03-08 17:16:59 +0200

    Add license info to pom

--- build.clj
+++ build.clj
@@ -29,7 +29,11 @@
                 :src-dirs ["src"]
                 :scm {:url "https://github.com/tatut/ripley"
                       :connection "scm:git:git://github.com/tatut/ripley.git"
-                      :tag (commit-sha)}})
+                      :tag (commit-sha)}
+                :pom-data  [[:licenses
+                             [:license
+                              [:name "MIT License"]
+                              [:url "https://opensource.org/license/mit"]]]]})
   (b/copy-dir {:src-dirs ["src" "resources"]
                :target-dir class-dir})
   (b/jar {:class-dir class-dir
